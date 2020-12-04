5ae39c686b7a1864226d2154e4f560234936ccc5
edwardp22 <edwardp22@hotmail.com>
2020-12-04 05:48:47 -0400

    New home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import classes from './Home.module.css';
 
 export default function Home() {
     return (
-        <div>ffff</div>
+        <div className={classes.background}>
+            <div className={classes.container}>
+                <div className={classes.data}>
+                    <h1>Edward Peña</h1>
+                </div>
+            </div>
+        </div>
     )
 }
