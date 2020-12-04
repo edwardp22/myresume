91f0701f2a11edc1f283854c6ac42d7687e28dea
edwardp22 <edwardp22@hotmail.com>
2020-12-04 14:39:17 -0400

    Update Menu.js
    Default values

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,26 +1,25 @@
 import React, { useContext } from 'react';
 import classes from './Menu.module.css';
-import { Link } from '../ALink/ALink';
+import Link from '../ALink/ALink';
 import { generalContext } from '../Main';
 
-export default function Menu({ toggleMenu }) {
-    const { links } = useContext(generalContext);
+export default function Menu({ toggleMenu = ()=>{} }) {
+    const { links, activeLink } = useContext(generalContext);
 
     return (
-        <ul className={classes.ul}>            
-            {links.map(link => {                    
-                return (
-                    <li key={link.text}>
-                        <Link 
-                            borderRadius='50px' 
-                            text={link.text} 
-                            onClick={toggleMenu} 
-                            icon={link.icon}
-                            linkRoute={link.link}
-                        />
-                    </li>
-                )
-            })}
+        <ul className={classes.ul}>
+            {links.map(link =>
+                <li key={link.text}>
+                    <Link
+                        borderRadius='50px'
+                        text={link.text}
+                        onClick={toggleMenu}
+                        icon={link.icon}
+                        linkRoute={link.link}
+                        active={activeLink === link.link}
+                    />
+                </li>
+            )}
         </ul>
     )
 }
