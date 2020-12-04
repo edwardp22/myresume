94e749243d609b39e25dc96755eac5b39450d026
edwardp22 <edwardp22@hotmail.com>
2020-12-04 11:25:33 -0400

    Home information

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import classes from './Home.module.css';
+import { LinkedIn } from '@material-ui/icons';
+import { IconButton } from '@material-ui/core';
 
 export default function Home() {
     return (
-        <div className={classes.background}>
-            <div className={classes.container}>
-                <div className={classes.data}>
-                    <h1>Edward Peña</h1>
-                </div>
+        <div className={classes.container}>
+            <div className={classes.data}>
+                <h1 className={classes.name}>Edward Peña</h1>
+                <h2 className={classes.title}>Web Developer</h2>
+                <IconButton size='small' className={classes.linkedin}>
+                    <LinkedIn fontSize='default' />
+                </IconButton>
             </div>
         </div>
     )
