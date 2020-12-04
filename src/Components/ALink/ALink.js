512718cdc561ee02924a71b25bf7156172a13e87
edwardp22 <edwardp22@hotmail.com>
2020-12-04 14:26:30 -0400

    Update ALink.js
    Implementing proptypes and making it independent

--- src/Components/ALink/ALink.js
+++ src/Components/ALink/ALink.js
@@ -1,24 +1,32 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import classes from './ALink.module.css';
+import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
-import { generalContext } from '../Main';
+
+Link.propTypes = {
+    text: PropTypes.string,
+    icon: PropTypes.string,
+    borderRadius: PropTypes.string,
+    onClick: PropTypes.func,
+    linkRoute: PropTypes.string,
+    active: PropTypes.bool
+}
 
 /**
- * Customizable button
+ * Customizable Link
  * @param {String} text text to show
  * @param {String} icon icon to show
  * @param {String} borderRadius is a rounded button
  * @param {Function} onClick function onClick
  * @param {String} linkRoute link of the page
+ * @param {Boolean} active active link
  */
-export const Link = ({ text, icon, borderRadius = '0px', onClick, linkRoute }) => {
-    const { activeLink } = useContext(generalContext);
-
+export default function Link({ text, icon, borderRadius = '0px', onClick, linkRoute, active }) {
     return (
         <NavLink
             style={{ borderRadius: borderRadius }}
-            className={[classes.link, (activeLink === linkRoute ? classes.active : '')].join(' ')}
-            onClick={() => window.innerWidth < 600 ? onClick() : null}
+            className={[classes.link, (active ? classes.active : '')].join(' ')}
+            onClick={() => window.innerWidth < 600 ? onClick ? onClick() : null : null}
             to={linkRoute}
         >
             <i className={[classes.icon, 'material-icons'].join(' ')}>{icon}</i>
