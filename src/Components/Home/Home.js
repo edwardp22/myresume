ca1a211369e2cf0b234cf6a72ca8e2113575f2db
edwardp22 <edwardp22@hotmail.com>
2020-12-05 07:33:39 -0400

    Adding technologies icons

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import classes from './Home.module.css';
+import classesIcons from './Icons.module.css';
 import { LinkedIn } from '@material-ui/icons';
-import { IconButton } from '@material-ui/core';
+import { IconButton, Grid } from '@material-ui/core';
+import { techIcons } from './Icons';
 
 export default function Home() {
     return (
-        <div className={classes.container}>
-            <div className={classes.data}>
-                <h1 className={classes.name}>Edward Peña</h1>
-                <h2 className={classes.title}>Movil - Web Developer</h2>
-                <IconButton size='small' className={classes.linkedin} onClick={() => window.open('https://www.linkedin.com/in/edward-abraham-peña-espinal-176aa965', '_blank')}>
-                    <LinkedIn fontSize='default' />
-                </IconButton>
-            </div>
-        </div>
+        <Grid container className={classes.container}>
+            <Grid container item xs={12} md={8} alignItems='center'>
+                <Grid item className={classes.data}>
+                    <h1 className={classes.name}>Edward Peña</h1>
+                    <h2 className={classes.title}>Movil - Web - DeskTop Developer</h2>
+                    <IconButton size='small' className={classesIcons.linkedin} onClick={() => window.open('https://www.linkedin.com/in/edward-abraham-peña-espinal-176aa965', '_blank')}>
+                        <LinkedIn fontSize='default' />
+                    </IconButton>
+                </Grid>
+            </Grid>
+
+            <Grid container item xs={12} md={4} alignItems='center'>
+                <Grid item className={classes.data}>
+                    {techIcons.map(icon =>
+                        <IconButton 
+                            size='medium'
+                            onClick={() => window.open(icon.src, '_blank')}
+                            className={classesIcons[icon.className]}
+                        >
+                            <img src={icon.img} alt={icon.alt} width="50" height="50" />
+                        </IconButton>
+                    )}
+                </Grid>
+            </Grid>
+        </Grid>
     )
 }
