c406cec729de6966f2e0534a927c4650d1f69b35
edwardp22 <edwardp22@hotmail.com>
2020-12-06 12:51:19 -0400

    Implementing typescript

---
+++ src/Components/TopBar/TopBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './TopBar.module.css';
+import { Menu, Close } from '@material-ui/icons';
+import { IconButton } from '@material-ui/core';
+
+export default function TopBar(
+    { isMenuClosed = false, toggleMenu } :
+    { isMenuClosed?: Boolean, toggleMenu: Function }
+) : JSX.Element {
+    return (
+        <IconButton size='medium' className={classes.menuButton} onClick={() => toggleMenu()}>
+            {isMenuClosed ? <Menu fontSize='default' /> : <Close fontSize='default' />}
+        </IconButton>
+    )
+}
