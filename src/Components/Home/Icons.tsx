126daa26cc555829d48acc969c5e418e953f3b96
edwardp22 <edwardp22@hotmail.com>
2021-04-12 08:26:45 -0400

    Update Icons.tsx
    Pongo la ruta de git hub para encontrar iconos.

--- src/Components/Home/Icons.tsx
+++ src/Components/Home/Icons.tsx
@@ -1,104 +1,104 @@
 export const techIcons = [
-    {
-        src: 'https://developer.mozilla.org/es/docs/Web/HTML',
-        className: 'html',
-        img: '/Home/html.png',
-        alt: 'HTML'
-    },
-    {
-        src: 'https://developer.mozilla.org/es/docs/Web/CSS',
-        className: 'css',
-        img: '/Home/css.png',
-        alt: 'CSS'
-    },
-    {
-        src: 'https://developer.mozilla.org/es/docs/Learn/JavaScript/First_steps/Qu%C3%A9_es_JavaScript',
-        className: 'javascript',
-        img: '/Home/javascript.png',
-        alt: 'JavaScript'
-    },
-    {
-        src: 'https://reactjs.org/',
-        className: 'react',
-        img: '/Home/react.png',
-        alt: 'React JS'
-    },
-    {
-        src: 'https://reactnative.dev/',
-        className: 'reactnative',
-        img: '/Home/react-native.png',
-        alt: 'React Native'
-    },
-    {
-        src: 'https://redux.js.org/',
-        className: 'redux',
-        img: '/Home/redux.png',
-        alt: 'React Redux'
-    },
-    {
-        src: 'https://www.typescriptlang.org/',
-        className: 'typescript',
-        img: '/Home/typescript.png',
-        alt: 'TypeScript'
-    },
-    {
-        src: 'https://getbootstrap.com/',
-        className: 'bootstrap',
-        img: '/Home/bootstrap.png',
-        alt: 'Bootstrap'
-    },
-    {
-        src: 'https://material-ui.com/',
-        className: 'materialui',
-        img: '/Home/materialui.png',
-        alt: 'Material UI'
-    },
-    {
-        src: 'https://www.embarcadero.com/es/products/delphi',
-        className: 'delphi',
-        img: '/Home/delphi.png',
-        alt: 'Delphi'
-    },
-    {
-        src: 'https://www.microsoft.com/es-es/sql-server/sql-server-2019',
-        className: 'sqlserver',
-        img: '/Home/sqlserver.png',
-        alt: 'SQL Server'
-    },
-    {
-        src: 'https://www.mongodb.com/',
-        className: 'mongodb',
-        img: '/Home/montodb.png',
-        alt: 'MongoDB'
-    },
-    {
-        src: 'https://nodejs.org/en/',
-        className: 'nodejs',
-        img: '/Home/nodejs.png',
-        alt: 'Node JS'
-    },
-    {
-        src: 'https://expressjs.com/',
-        className: 'expressjs',
-        img: '/Home/expressjs.png',
-        alt: 'Express JS'
-    },
-    {
-        src: 'https://git-scm.com/',
-        className: 'git',
-        img: '/Home/git.png',
-        alt: 'Git'
-    },
-    {
-        src: 'https://github.com/',
-        className: 'github',
-        img: '/Home/github.png',
-        alt: 'GitHub'
-    },
-    {
-        src: 'https://azure.microsoft.com/en-us/',
-        className: 'azure',
-        img: '/Home/azure.png',
-        alt: 'Azure'
-    }
-]
+  {
+    src: "https://developer.mozilla.org/es/docs/Web/HTML",
+    className: "html",
+    img: "/myresume/Home/html.png",
+    alt: "HTML",
+  },
+  {
+    src: "https://developer.mozilla.org/es/docs/Web/CSS",
+    className: "css",
+    img: "/myresume/Home/css.png",
+    alt: "CSS",
+  },
+  {
+    src: "https://developer.mozilla.org/es/docs/Learn/JavaScript/First_steps/Qu%C3%A9_es_JavaScript",
+    className: "javascript",
+    img: "/myresume/Home/javascript.png",
+    alt: "JavaScript",
+  },
+  {
+    src: "https://reactjs.org/",
+    className: "react",
+    img: "/myresume/Home/react.png",
+    alt: "React JS",
+  },
+  {
+    src: "https://reactnative.dev/",
+    className: "reactnative",
+    img: "/myresume/Home/react-native.png",
+    alt: "React Native",
+  },
+  {
+    src: "https://redux.js.org/",
+    className: "redux",
+    img: "/myresume/Home/redux.png",
+    alt: "React Redux",
+  },
+  {
+    src: "https://www.typescriptlang.org/",
+    className: "typescript",
+    img: "/myresume/Home/typescript.png",
+    alt: "TypeScript",
+  },
+  {
+    src: "https://getbootstrap.com/",
+    className: "bootstrap",
+    img: "/myresume/Home/bootstrap.png",
+    alt: "Bootstrap",
+  },
+  {
+    src: "https://material-ui.com/",
+    className: "materialui",
+    img: "/myresume/Home/materialui.png",
+    alt: "Material UI",
+  },
+  {
+    src: "https://www.embarcadero.com/es/products/delphi",
+    className: "delphi",
+    img: "/myresume/Home/delphi.png",
+    alt: "Delphi",
+  },
+  {
+    src: "https://www.microsoft.com/es-es/sql-server/sql-server-2019",
+    className: "sqlserver",
+    img: "/myresume/Home/sqlserver.png",
+    alt: "SQL Server",
+  },
+  {
+    src: "https://www.mongodb.com/",
+    className: "mongodb",
+    img: "/myresume/Home/montodb.png",
+    alt: "MongoDB",
+  },
+  {
+    src: "https://nodejs.org/en/",
+    className: "nodejs",
+    img: "/myresume/Home/nodejs.png",
+    alt: "Node JS",
+  },
+  {
+    src: "https://expressjs.com/",
+    className: "expressjs",
+    img: "/myresume/Home/expressjs.png",
+    alt: "Express JS",
+  },
+  {
+    src: "https://git-scm.com/",
+    className: "git",
+    img: "/myresume/Home/git.png",
+    alt: "Git",
+  },
+  {
+    src: "https://github.com/",
+    className: "github",
+    img: "/myresume/Home/github.png",
+    alt: "GitHub",
+  },
+  {
+    src: "https://azure.microsoft.com/en-us/",
+    className: "azure",
+    img: "/myresume/Home/azure.png",
+    alt: "Azure",
+  },
+];
